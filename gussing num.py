99a075c4453a072efd25753967b4c7c4d4c945e8
joudykarim <dodyjoudy70@gmail.com>
2025-09-20 08:39:43 -0700

    edits

--- gussing num.py
+++ gussing num.py
@@ -1,3 +1,9 @@
 import random
+gn=int(input("enter ur gn="))
 gussingn=import.randint(1,100)
-gnn=int(input("enter ur gn="))
+if gussingn==gn:
+    print("congrtulationss")
+if gussingn>gn:
+    print("too low")
+if gussingn<gn:
+    print("too high")
